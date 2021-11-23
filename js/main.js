913cb84f7f77ec1eaa83918287af1450107005d3
madikirkman <madikirkman@users.noreply.github.com>
2021-11-23 04:32:21 +0000

    deploy: c620d8a98486cee6deccff27e8f10d848c7f8286

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ window.onclick = function (event) {
 /* DIVIDE VERSION */
 
 /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+toggle between hiding and showing the dropdown content  */
 function myFunctionDivide() {
   document.getElementById("myDropdownDivide").classList.toggle("show");
 }
@@ -100,3 +100,82 @@ window.onclick = function (event) {
     }
   }
 };
+/*
+var slideIndexPlus = 1;
+showSlidesPlus(slideIndexPlus);
+
+// Next/previous controls
+function plusSlidesplus(n) {
+  showSlidesPlus((slideIndexPlus += n));
+}
+
+// Thumbnail image controls
+function currentSlidePlus(n) {
+  showSlidesPlus((slideIndexPlus = n));
+}
+
+function showSlidesPlus(n) {
+  var i;
+  var slidesplus = document.getElementsByClassName("mySlidesPlus");
+  var dotsplus = document.getElementsByClassName("dotplus");
+  if (n > slidesplus.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slidesplus.length;
+  }
+  for (i = 0; i < slidesplus.length; i++) {
+    slidesplus[i].style.display = "none";
+  }
+  for (i = 0; i < dotsplus.length; i++) {
+    dotsplus[i].className = dotsplus[i].className.replace(" activeplus", "");
+  }
+  slidesplus[slideIndexPlus - 1].style.display = "block";
+  dotsplus[slideIndexPlus - 1].className += " activeplus";
+}
+*/
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
+
+var slideIndex = [1, 1, 1, 1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+function plusSlides(n, no) {
+  showSlides((slideIndex[no] += n), no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+}
